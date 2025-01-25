4d2ebedf0d986fd0c6ad8e8a8126218dc77ff51c
RoronoaZoroDAce <90967650+RoronoaZoroDAce@users.noreply.github.com>
2025-01-25 17:36:31 +0530

    Add global state for auth & routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 
