543d67a58f9557f44a2ec5e33a15ea148534756a
RoronoaZoroDAce <90967650+RoronoaZoroDAce@users.noreply.github.com>
2025-01-25 14:12:07 +0530

    Added Initial Routing system and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
 // import React from 'react'
 
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
